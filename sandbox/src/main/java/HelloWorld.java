eb78a2d243bddb9f2bc209e6636c4d4649ad2d6e
qaokss <qa.okss@gmail.com>
2019-03-03 21:29:05 +0300

    перименован класс Hello в HelloWorld

---
+++ sandbox/src/main/java/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld, world!");
+        System.out.println(2 + 2);
+        System.out.println(2 - 2);
+        System.out.println(2 / 2);
+        System.out.println(2 - 2);
+
+        System.out.println(1 / 2);
+        System.out.println(1.0 / 2);
+
+        System.out.println("2" + "2");
+        System.out.println("2" + 2);
+        System.out.println("2 + 2 = " + (2 + 2));
+    }
+}
