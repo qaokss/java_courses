638b4c44857e4799204d77da3d3b67f2928a3047
qaokss <qa.okss@gmail.com>
2019-03-09 18:57:55 +0300

    добавлено несколько функций

--- sandbox/src/main/java/HelloWorld.java
+++ sandbox/src/main/java/HelloWorld.java
@@ -1,16 +1,32 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld, world!");
-        System.out.println(2 + 2);
-        System.out.println(2 - 2);
-        System.out.println(2 / 2);
-        System.out.println(2 - 2);
+        hello("world");
+        hello("user");
 
-        System.out.println(1 / 2);
-        System.out.println(1.0 / 2);
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
 
-        System.out.println("2" + "2");
-        System.out.println("2" + 2);
-        System.out.println("2 + 2 = " + (2 + 2));
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со второнами " + a + " и " + b + " = " + area(a, b));
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+
+
+//      рассчитывается квадрат значения и выводится на консоль
+//        double side = 6.5;
+//        double quadr = side * side;
+//        System.out.println("Площадь квадрата со стороной " + side + " = " + quadr);
+    }
+
+    public static double area (double side) {
+        return side * side;
+    }
+
+    public static double area (double a, double b) {
+        return a * b;
     }
 }
+
