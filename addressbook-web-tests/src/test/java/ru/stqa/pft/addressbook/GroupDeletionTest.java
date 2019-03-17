b97d4e5922702132e58e9e944919df87215eb8c6
qaokss <qa.okss@gmail.com>
2019-03-17 17:36:07 +0300

    новыйц тест, проверяющий удаление группы

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTest.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook;
+
+import org.testng.annotations.Test;
+
+
+public class GroupDeletionTest extends TestBase {
+
+    @Test
+    public void testGroupDeletion() throws Exception {
+        gotoGroupPage();
+        selectGroup();
+        deleteSelectedGroup();
+        gotoGroupPage();
+    }
+
+
+}
