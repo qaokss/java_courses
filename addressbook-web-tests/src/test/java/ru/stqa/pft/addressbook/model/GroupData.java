9589830365174cd00622245b300249d62fb0362b
qaokss <qa.okss@gmail.com>
2019-04-08 23:17:04 +0300

    добавлена проверка до-после в тест GroupModification

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,42 +3,67 @@ package ru.stqa.pft.addressbook.model;
 
 import java.util.Objects;
 
-public  class GroupData {
-        public  String name;
-        public   String header;
-        public   String footer;
+public class GroupData {
+    private final String id;
+    private final String name;
+    private final String header;
+    private final String footer;
 
 
-        public GroupData (String name, String header, String footer) {
-            this.name = name;
-            this.header = header;
-            this.footer = footer;
-        }
 
-        public String getName() { return name; }
 
-        public String getHeader() { return header; }
+    public GroupData(String id, String name, String header, String footer) {
+        this.id = id;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
 
-        public String getFooter() { return footer; }
 
-        @Override
-        public String toString() {
-            return "GroupData{" +
-                    "name='" + name + '\'' +
-                    '}';
-        }
+    public GroupData(String name, String header, String footer) {
+        this.id = null;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getFooter() {
+        return footer;
+    }
+
+    public String getId() {
+        return id;
+    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return Objects.equals(name, groupData.name);
+        return Objects.equals(name, groupData.name) &&
+                Objects.equals(id, groupData.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name);
+        return Objects.hash(name, id);
     }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
+
 }
 
