e9ece9eed59f223b5e0f8ee78c69f8915c46dd04
qaokss <qa.okss@gmail.com>
2019-04-13 16:45:13 +0300

    добавлен метод, формирующий список контактов на главной странице

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -2,11 +2,16 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContactHelper extends HelperBase {
+
 
     public ContactHelper(WebDriver wd) {
         super(wd);
@@ -16,9 +21,7 @@ public class ContactHelper extends HelperBase {
         type(By.name("firstname"), contactData.getFirstname());
         type(By.name("middlename"), contactData.getMiddlename());
         type(By.name("lastname"), contactData.getLastname());
-        type(By.name("nickname"), contactData.getNickname());
         type(By.name("title"), contactData.getTitle());
-        type(By.name("company"), contactData.getNamecompany());
         type(By.name("address"), contactData.getAddress());
         type(By.name("mobile"), contactData.getMobilephone());
         type(By.name("email"), contactData.getEmail1());
@@ -46,11 +49,11 @@ public class ContactHelper extends HelperBase {
         click(By.xpath("(//input[@name='update'])[2]"));
     }
 
-    public void chooseCheckboxContact() {
+    public void chooseCheckboxContact(int index) {
         click((By.name("selected[]")));
     }
 
-    public void initContactDeletion () {
+    public void initContactDeletion() {
         click(By.xpath("//input[@value='Delete']"));
     }
 
@@ -65,5 +68,20 @@ public class ContactHelper extends HelperBase {
     public void createNewContact(ContactData contactData, boolean creation) {
         fillNewContact(contactData, creation);
         submitContactCreation();
+    }
+
+    // формируем список контактов на странице
+    public List<ContactData> getContactList() {
+        List<ContactData> contacts = new ArrayList<>();
+        List<WebElement> rows = wd.findElements(By.name("entry"));
+        for (WebElement row : rows) {
+            String lastName = row.findElement(By.xpath("//td[2]")).getText();
+            String firstName = row.findElement(By.xpath("//td[3]")).getText();
+
+            int id = Integer.parseInt(row.findElement(By.tagName("input")).getAttribute("value"));
+            ContactData contact = new ContactData(id, lastName, firstName);
+            contacts.add(contact);
+        }
+        return contacts;
     }
 }
