d0bdcbb680103cb466fa5d3685319d839175a7c9
qaokss <qa.okss@gmail.com>
2019-04-14 18:31:33 +0300

    реализован тест модификации группы путём сравнения отсортированных списков

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -41,8 +41,8 @@ public class ContactHelper extends HelperBase {
         click(By.xpath("(//input[@name='submit'])[2]"));
     }
 
-    public void initContactModification() {
-        click(By.xpath("//img[@alt='Edit']"));
+    public void initContactModification(int index) {
+        wd.findElements(By.xpath("//img[@alt='Edit']")).get(index).click();
     }
 
     public void submitContactModification() {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
@@ -1,31 +1,43 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class ContactModificationTest extends TestBase {
 
     @Test
     public void testContactModification() {
-    app.getNavigationHelper().gotoHomePage();
-        if (! app.getContactHelper().isThereAContact()) {
+        app.getNavigationHelper().gotoHomePage();
+        if (!app.getContactHelper().isThereAContact()) {
             app.getNavigationHelper().gotoAddContactPage();
             app.getContactHelper().createNewContact(new ContactData("TestFirtsName", "Middlename",
-                    "Lastname", "Nickname", "Title", "Tne Best Company", "London",
-                    "123456798", "5553535", "", "", "qwe@as.fg",
-                    "", "", "qwerty.xx.ss", "21", "December",
-                    "2000", "", "", "", "test1", "",
-                    "", ""), true);
+                    "Lastname", "Nickname", "adr", "5678", "London@fg.tyh",
+                    "", "10", "January", "2000", ""), true);
             app.getNavigationHelper().gotoHomePage();
         }
-    app.getContactHelper().initContactModification();
-    app.getContactHelper().fillNewContact(new ContactData("Edited FirtsName", "Edited Middlename",
-            "Edited Lastname", "Edited Nickname", "Edited Title", "Edited Tne Best Company",
-            "London", "666", "12456", "55555", "", "qwe666@as.fg",
-            "", "", "qwerty.xx.ss", "10", "June",
-            "1966", "", "", "", null, "", "", ""),
-            false);
-    app.getContactHelper().submitContactModification();
-    app.getNavigationHelper().gotoHomePage();
+        List<ContactData> before = app.getContactHelper().getContactList();
+        ContactData contact = new ContactData(before.get(before.size() - 1).getId(), "Edited FirtsName", "Edited Middlename",
+                "Edited Lastname", "Edited Title", "edited adr", "87878787",
+                "edited@mail.ee", "", "11", "December", "1988",
+                "");
+        app.getContactHelper().initContactModification(before.size() - 1);
+        app.getContactHelper().fillNewContact(contact, false);
+        app.getContactHelper().submitContactModification();
+        app.getNavigationHelper().gotoHomePage();
+        List<ContactData> after = app.getContactHelper().getContactList();
+        Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(contact);
+
+        Comparator<? super ContactData> byId = Comparator.comparingInt(ContactData::getId);
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
+
     }
 }
