698e565d1bc437987b01e37ee67e81788dad5587
qaokss <qa.okss@gmail.com>
2019-04-16 23:03:19 +0300

    5.1 - 5.6 для теста создания контакта реализованы проверки в fluent-стиле, используя Hamcrest

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -1,39 +1,36 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.Assert;
+
 import org.testng.annotations.*;
-
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
 
-import java.util.Comparator;
-import java.util.List;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
+
+
 
 public class ContactCreationTest extends TestBase {
 
     @Test
     public void testContactCreation() throws Exception {
         app.goTo().homePage();
-        List<ContactData> before = app.getContactHelper().getContactList();
+        Contacts before = app.contact().allContacts();
 
+        ContactData contact = new ContactData().withFirstname("TestFirtsName").withMiddlename("Middlename").
+                withLastname("Lastname").withAddress("adr").withMobilephone("4654654").withEmail1("London@fg.tyh").
+                withGroup("000"). withBirthdayDay("10").withBirthdayMonth("January").withBirthdayYear("2000").
+                withTitle("Tittle").withHomepage("qwerty@piu.piu");
         app.goTo().addContactPage();
-        ContactData contact = new ContactData("TestFirtsName", "Middlename",
-                "Lastname", "Tittle", "adr", "5678", "London@fg.tyh",
-                "", "10", "January", "2000", "000");
-        app.getContactHelper().createNewContact(contact, true);
-        app.goTo().homePage();
-        List<ContactData> after = app.getContactHelper().getContactList();
-        Assert.assertEquals(after.size(), before.size() + 1);
+        app.contact().createNew(contact,true);
 
+        Contacts after = app.contact().allContacts();
 
-        // находим и подставляем максимальный id в список с помощью лямбда-выражения
-        contact.setId(after.stream().max(Comparator.comparingInt(ContactData::getId)).get().getId());
-        before.add(contact);
+        assertEquals(after.size(), before.size() + 1);
 
-        // сравниваем отсортированные списки
-        Comparator<? super ContactData> byId = Comparator.comparingInt(ContactData::getId);
-        before.sort(byId);
-        after.sort(byId);
-        Assert.assertEquals(before, after);
+        assertThat(after,
+                equalTo( before.withAdded(contact.withId(after.stream().mapToInt( (g) -> g.getId()).max().getAsInt()))));
 
 
     }
