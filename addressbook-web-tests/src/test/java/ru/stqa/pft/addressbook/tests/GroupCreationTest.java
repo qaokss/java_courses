e69458767eff62220b7ee5b0cc2be5f114a7ac15
qaokss <qa.okss@gmail.com>
2019-04-21 17:11:33 +0300

    6.4 параметризирован метод теста создангия группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -6,19 +6,31 @@ import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTest extends TestBase {
 
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<>();
+        list.add(new Object[] {new GroupData().withName("group name 1").withHeader("header 1").withFooter("footer 1")});
+        list.add(new Object[] {new GroupData().withName("group name 2").withHeader("header 2").withFooter("footer 2")});
+        list.add(new Object[] {new GroupData().withName("group name 2").withHeader("header 3").withFooter("footer 3")});
+        return list.iterator();
+    }
+
     /**
      * Тест проверяет корректное создание группы
      */
-    @Test
-    public void testGroupCreation() throws Exception {
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) throws Exception {
         app.goTo().groupPage();
         Groups before = app.group().allGroups();
-        GroupData group = new GroupData().withName("testName").withFooter("testFooter").withHeader("testHeader");
         app.group().create(group);
         assertThat(app.group().count(), equalTo(before.size() + 1));
         Groups after = app.group().allGroups();
@@ -36,7 +48,7 @@ public class GroupCreationTest extends TestBase {
         GroupData group = new GroupData().withName("testName'").withFooter("testFooter").withHeader("testHeader");
         app.group().create(group);
         assertThat(app.group().count(), equalTo(before.size()));
-        Groups after = (Groups) app.group().allGroups();
+        Groups after = app.group().allGroups();
         assertThat(after, equalTo(before));
     }
 
