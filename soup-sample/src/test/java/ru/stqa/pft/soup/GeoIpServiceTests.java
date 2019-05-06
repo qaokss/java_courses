79d55514cacabb94a8ee6392ee206b9571d1feb6
qaokss <qa.okss@gmail.com>
2019-05-06 23:14:12 +0300

    9.2 добавлен wsdl для взаимодействия с soap

---
+++ soup-sample/src/test/java/ru/stqa/pft/soup/GeoIpServiceTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.soap;
+
+import com.lavasoft.GeoIPService;
+import org.testng.annotations.Test;
+
+
+public class GeoIpServiceTests {
+
+    @Test
+    public void testMyIp() {
+        String ipLocation = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("46.146.16.99");
+        System.out.println("ipLocation " + ipLocation);
+        //String ipLocation = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("46.146.16.99");
+        //assertEquals(ipLocation,"RUS");
+        //MatcherAssert.(ipLocation, hasItem("RU"));
+        // assertThat(ipLocation, "RU");
+
+    }
+}
